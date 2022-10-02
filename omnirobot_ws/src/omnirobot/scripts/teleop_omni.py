e71a1d618ce31d8205491e3bc17ab20ca881588e
erick <erick.suzart@ufba.br>
2022-10-02 08:08:06 -0300

    Teleopnode updated

--- omnirobot_ws/src/omnirobot/scripts/teleop_omni.py
+++ omnirobot_ws/src/omnirobot/scripts/teleop_omni.py
@@ -11,7 +11,7 @@ Reading from the keyboard
 Moving around:
    u    i    o
    j    k    l
-   m    ,    .
+        ,    
 
 q/z : increase/decrease max speeds by 1 unit
 
@@ -23,13 +23,11 @@ a = sqrt(3).real/3
 # v_0, v_1, v_2 are the velocities of the three wheels
 moveBindings = {
   'i':(-a, 0,a),
-  'o':(1,-1,0),
+  'o':(-1,-1,-1),
   'j':(-1/3,2/3,-1/3),
   'l':(1/3,-2/3,1/3),
-  'u':(-1,0,1),
-  ',':(a, 0,-a),
-  '.':(1,0,-1),
-  'm':(-1,1,0),  
+  'u':(1,1,1),  
+  ',':(a, 0,-a),  
 }
 
 speedBindings = {
@@ -54,19 +52,19 @@ if __name__=="__main__":
   x = 0
   y = 0
   z = 0
-  speed = float(10.0)
+  speed = 10.0
   print(msg)
 
   while(1):  
     key = getKey()   
 
     if key in moveBindings.keys():
-      x = float(moveBindings[key][0])
-      y = float(moveBindings[key][1])
-      z = float(moveBindings[key][2])
+      x = moveBindings[key][0]
+      y = moveBindings[key][1]
+      z = moveBindings[key][2]
 
     elif key in speedBindings.keys():
-      speed += float(speedBindings[key])
+      speed += speedBindings[key]
 
     else:
       x = 0
@@ -76,9 +74,9 @@ if __name__=="__main__":
     if (key == '\x03'):
       break  
 
-    vel = float(x * speed)
-    vel1 = float(y * speed)
-    vel2 = float(z * speed)
+    vel = Float64(x * speed)
+    vel1 = Float64(y * speed)
+    vel2 = Float64(z * speed)
 
     pub.publish(vel)
     pub1.publish(vel1)
