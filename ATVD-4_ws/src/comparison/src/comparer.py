cbc2c04a6ecc196052c88b0afdff377a409da44a
Pedro Augusto Dultra Neves Correia <pedro.correia@solutis.com.br>
2022-11-09 16:18:36 -0300

    update comparer.py

--- ATVD-4_ws/src/comparison/src/comparer.py
+++ ATVD-4_ws/src/comparison/src/comparer.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from geometry_msgs.msg  import Twist, Point
+from geometry_msgs.msg  import Twist, Point, Pose2D
 from sensor_msgs.msg  import Imu
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from math import cos,sin
 from gazebo_msgs.srv import GetModelState
+import rosbag
+outbag = rosbag.Bag('output.bag', 'w')
 
-
+pub1 = rospy.Publisher('/diff_filtered_odometry', Pose2D, queue_size = 15)
+pub2 = rospy.Publisher('/diff_odometry', Pose2D, queue_size = 15)
 def update_filtered_odometry(msg):
-    str = "hello world %s"%msg.pose.pose.position.x
     rospy.loginfo(str)
-    rospy.sleep(0.1)
+    rospy.sleep(0.5)
+    now = rospy.get_rostime()
+    gt = get_ground_truth()
+    yaw = get_rotation(msg.pose.pose.orientation)
+    #outbag.write('/filtered_odometry', Pose2D(msg.pose.pose.position.x, msg.pose.pose.position.y, yaw), now)
+    pub1.publish(Pose2D(msg.pose.pose.position.x - gt[0], msg.pose.pose.position.y - gt[1], yaw - gt[2]))
     return
 
 def update_odometry(msg):
-    str = "hello world2 %s"%msg.pose.pose.position.x
     rospy.loginfo(str)
-    rospy.sleep(0.1)
+    rospy.sleep(0.5)
+    now = rospy.get_rostime()
+    gt = get_ground_truth()
+    yaw = get_rotation(msg.pose.pose.orientation)
+    #outbag.write('/odometry', Pose2D(msg.pose.pose.position.x, msg.pose.pose.position.y, yaw), now)
+    pub2.publish(Pose2D(msg.pose.pose.position.x - gt[0], msg.pose.pose.position.y - gt[1], yaw - gt[2]))
     return
+
+def get_ground_truth():
+    g_get_state = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
+    rospy.wait_for_service("/gazebo/get_model_state")
+    try:
+        state = g_get_state(model_name="husky")
+    except Exception(e):
+        rospy.logerr('Error on calling service: %s',str(e))
+        return
+    x_gt = state.pose.position.x
+    y_gt = state.pose.position.y 
+    yaw_gt = get_rotation(state.pose.orientation)
+    return [x_gt, y_gt, yaw_gt]
 
 def comparer():
     pub_gt = rospy.Publisher('/gt', Point, queue_size=10)
@@ -27,21 +51,8 @@ def comparer():
     rospy.Subscriber("/odometry/filtered", Odometry, update_filtered_odometry)
     rospy.Subscriber("/husky_velocity_controller/odom", Odometry, update_odometry)
     while not rospy.is_shutdown():
-        g_get_state = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
-        rospy.wait_for_service("/gazebo/get_model_state")
-        try:
-            state = g_get_state(model_name="husky")
-        except Exception(e):
-            rospy.logerr('Error on calling service: %s',str(e))
-            return
-        x_gt = state.pose.position.x
-        y_gt = state.pose.position.y 
-        yaw_gt = get_rotation(state.pose.orientation)
-        theta_gt = yaw_gt 
-        pub_gt.publish(Point(x_gt, y_gt, yaw_gt))
-        str = "hello world3 %s"%x_gt
         rospy.loginfo(str)
-        rospy.sleep(0.1)
+        rospy.sleep(0.5)
  
 def get_rotation (msg):
     orientation_q = msg
