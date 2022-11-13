e1d6e833aa5b5eb6a0c139a7c408675e8f1f3473
And Paiva <andre.pcr@hotmail.com>
2022-11-13 16:27:32 -0300

    Refeitas simulacoes

---
+++ ATVD-3_ws/build/lar_gazebo/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/and/Github/ENGC68/ATVD-3_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/and/Github/ENGC68/ATVD-3_ws/build/lar_gazebo'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/and/Github/ENGC68/ATVD-3_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/and/Github/ENGC68/ATVD-3_ws/src/lar_gazebo:$ROS_PACKAGE_PATH"
